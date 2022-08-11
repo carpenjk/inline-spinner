36acdee33e78f95ba5828a7bc35dbbbd4189c04b
carpenjk <56424605+carpenjk@users.noreply.github.com>
2022-08-11 15:22:58 -0400

    fix: removed spinner to leave inline-spinner only

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# project-name
+# inline-spinner
--- src/spinner.js
+++
@@ -1,2 +0,0 @@
-export { default as Spinner } from './components/spinner/Spinner'
-export { default as SpinnerContext } from './components/spinner/Spinner'
